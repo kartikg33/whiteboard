23cd38120ad17a9ff49b2d90027e5c290d700484
Kartik Gohil <kartik_gohil@hotmail.com>
2015-06-29 11:16:00 +0100

    remove button now allows the containers you click on to be removed, not just the last one created. double click or click remove button again to leave mode

--- overlay.js
+++ overlay.js
@@ -17,12 +17,18 @@ var board = function(){
 
 	//----------EVENTS----------//
 	$(".overlay").dblclick(function(){
+		if(remove=="true"){
+			remove = "false";
+			$(".overlay").css('cursor', 'auto');
+		} else {
 		var num = "id"+$(".overlay").children().length;
 		jQuery("<div/>", {
 		    id: num,
 		}).appendTo(".overlay");
 		$("#"+num).load("box.htm");
+		}
 	});
+
 
 	$(".addbtn").click(function(){
 		var num = "id"+$(".overlay").children().length;
@@ -32,15 +38,26 @@ var board = function(){
 		$("#"+num).load("box.htm");
 	});
 
+	var remove = "false";
 	$(".rembtn").click(function(){
-		var num = "id"+($(".overlay").children().length-1);
-		$("#"+num).remove();
+		if(remove=="false"){
+			remove = "true";
+			$(".overlay").css('cursor', 'crosshair');
+			$(".container").css('cursor', 'crosshair');
+		} else {
+			remove = "false";
+			$(".overlay").css('cursor', 'auto');
+			$(".container").css('cursor', 'auto');
+		}
+		//var num = "id"+($(".overlay").children().length-1);
+		//$("#"+num).remove();
 	});
 
 	$(document).on("mouseover",".container",function(event) {
 		$(this).css({
-			'border': '1px dashed #742B32'
-		}); 
+			'border': '1px dashed #742B32',
+			'cursor': 'grab'
+		}); 	
 	});
 
 	$(document).on("mouseleave",".container",function(event) {
@@ -53,41 +70,52 @@ var board = function(){
 	var dragging = null;
 	$(document).on("mouseup",".overlay",function(e) {
 		$(document).off("mousemove",".container");
+		if(dragging!=null){
+			dragging.css('cursor','auto');
+		}
 		dragging = null;
 		$('.debug').text('DEBUG '+dragging);
 	});		
 
 	$(document).on("mousedown",".container",function(e) {
-		var position = $(this).offset();
+		
 		if(dragging==null){
 			dragging=$(this);	//to prevent other elements from being moved
+			var position = $(this).offset();
+			dragging.css('cursor','move');
+		
+			$(document).on("mousemove",".container",function(event){
+				
+				var move_left = Math.max($(".overlay").offset().left
+					, position.left + event.pageX - e.pageX);
+				move_left = Math.min(($(".overlay").offset().left+newwidth)-$(this).width()
+					,move_left); 
+
+				var move_top = Math.max($(".overlay").offset().top
+					, position.top + event.pageY - e.pageY);
+				move_top = Math.min(($(".overlay").offset().top+newheight)-$(this).height()
+					, move_top);
+
+				$('.debug').text('DEBUG pos: '+position.left+', '+position.top+'; '+
+							'click: '+e.pageX+', '+e.pageY+'; '+
+							'current: '+event.pageX+', '+event.pageY+'; '+
+							'new_pos: '+move_left+', '+move_top	+'; '+
+							dragging.parent().attr('id') 
+					);
+
+				dragging.css({	//only moves what is being dragged, not others by accident.
+					'position': 'fixed',
+					'left': move_left,
+					'top': 	move_top,
+				}); 
+
+			});
+		} 
+		if (remove=="true"){
+			//var num = "id"+($(".overlay").children().length-1);
+			//$("#"+num).remove();
+			$(this).parent().remove();
 		}
-		$(document).on("mousemove",".container",function(event){
-
-			var move_left = Math.max($(".overlay").offset().left
-				, position.left + event.pageX - e.pageX);
-			move_left = Math.min(($(".overlay").offset().left+newwidth)-$(this).width()
-				,move_left); 
-
-			var move_top = Math.max($(".overlay").offset().top
-				, position.top + event.pageY - e.pageY);
-			move_top = Math.min(($(".overlay").offset().top+newheight)-$(this).height()
-				, move_top);
-
-			$('.debug').text('DEBUG pos: '+position.left+', '+position.top+'; '+
-						'click: '+e.pageX+', '+e.pageY+'; '+
-						'current: '+event.pageX+', '+event.pageY+'; '+
-						'new_pos: '+move_left+', '+move_top	+'; '+
-						dragging.parent().attr('id') 
-				);
-
-			dragging.css({	//only moves what is being dragged, not others by accident.
-				'position': 'fixed',
-				'left': move_left,
-				'top': 	move_top
-			}); 
-
-		});
 	});
 	
 
