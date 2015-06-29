919f851521d818b1ab8dd131317bf26a7bb95bcc
Kartik Gohil <kartik_gohil@hotmail.com>
2015-06-29 03:34:22 +0100

    now can drag one element across others without affecting their positions

--- overlay.js
+++ overlay.js
@@ -1,30 +1,32 @@
 var board = function(){
+	//----------INITIALISE PAGE----------//
+	var newwidth;
+	var newheight;
+	var initSize = function(){
+		newwidth = 0.9*$(window).width();
+		newheight = (1080/1920)*newwidth;
 
-	var newwidth = 0.9*$(window).width();
-	var newheight = (1080/1920)*newwidth;
+		$('.debug').text('DEBUG width: '+newwidth+', height: '+newheight);
 
-	$('.debug').text('width: '+newwidth+', height: '+newheight);
+		$(".overlay").width(newwidth);
+		$(".overlay").height(newheight);
+	}
 
-	$(".overlay").css({
-		'width': newwidth,
-		'height': newheight,
-		'top':'40',
-		'margin-left': 'auto',
-		'margin-right': 'auto',
-		'background-color': 'white'
-	});
+	initSize();
+	$(window).resize(initSize);
 
+	//----------EVENTS----------//
 	$(".addbtn").click(function(){
 		var num = "id"+$(".overlay").children().length;
 		jQuery("<div/>", {
-		    class: num,
+		    id: num,
 		}).appendTo(".overlay");
-		$("."+num).load("box.htm");
+		$("#"+num).load("box.htm");
 	});
 
 	$(".rembtn").click(function(){
 		var num = "id"+($(".overlay").children().length-1);
-		$("."+num).remove();
+		$("#"+num).remove();
 	});
 
 	$(document).on("mouseover",".container",function(event) {
@@ -40,14 +42,19 @@ var board = function(){
 	});
 
 
-	$(document).on("mouseup",".container",function(e) {
+	var dragging = null;
+	$(document).on("mouseup",".overlay",function(e) {
 		$(document).off("mousemove",".container");
+		dragging = null;
+		$('.debug').text('DEBUG '+dragging);
 	});		
 
 	$(document).on("mousedown",".container",function(e) {
 		var position = $(this).offset();
+		if(dragging==null){
+			dragging=$(this);	//to prevent other elements from being moved
+		}
 		$(document).on("mousemove",".container",function(event){
-			setTimeout(100);
 
 			var move_left = Math.max($(".overlay").offset().left
 				, position.left + event.pageX - e.pageX);
@@ -59,17 +66,19 @@ var board = function(){
 			move_top = Math.min(($(".overlay").offset().top+newheight)-$(this).height()
 				, move_top);
 
-			$('.debug').text('pos: '+position.left+', '+position.top+'; '+
+			$('.debug').text('DEBUG pos: '+position.left+', '+position.top+'; '+
 						'click: '+e.pageX+', '+e.pageY+'; '+
 						'current: '+event.pageX+', '+event.pageY+'; '+
-						'new_pos: '+move_left+', '+move_top		
+						'new_pos: '+move_left+', '+move_top	+'; '+
+						dragging.parent().attr('id') 
 				);
 
-			$(this).css({
+			dragging.css({	//only moves what is being dragged, not others by accident.
 				'position': 'fixed',
 				'left': move_left,
 				'top': 	move_top
 			}); 
+
 		});
 	});
 	
